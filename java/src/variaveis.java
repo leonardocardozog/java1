bedf298cd81cbf85a5538f4c03ad3d9e9f14dec7
leonado <leowatchloki@gmail.com>
2022-08-16 16:18:04 -0300

    new fille

---
+++ java/src/variaveis.java
@@ -0,0 +1,22 @@
+public class variaveis {
+
+    
+  public static void main(String[] args) {
+
+ byte idade = 123;
+short ano = 2021;
+int cep = 21070333;
+long cpf = 98765432109l;
+float pi = 3.14f; 
+double salariO = 1275.33;
+    
+System.out.println(+idade);
+System.out.println(+ano);
+System.out.println(+cep);
+System.out.println(+cpf);
+System.out.println(+pi);
+System.out.print(+salariO);
+
+  }
+
+}
