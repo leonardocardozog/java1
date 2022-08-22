ce509b188ba6bb3ff1a45db116c2fc037b687995
leonado <leowatchloki@gmail.com>
2022-08-22 16:57:32 -0300

    SmarTv

---
+++ java/src/SmaTv1.java
@@ -0,0 +1,35 @@
+public class SmaTv1 {
+
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+    
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    
+}
+}
---
+++ java/src/edu/leonardo/smarTv.java
@@ -0,0 +1,28 @@
+
+
+public class smarTv {
+    boolean ligada = true;
+
+    int canal = 1;
+    int volume = 25;
+    public void ligar () {
+       ligada=true; }
+
+public void desligar () {
+    ligada=false;}
+
+    public void aumentarVolume () {
+        volume++;
+    }
+        public void diminuirVolume (){
+            volume--;}
+public void aumentarCanal () {
+  canal ++;
+}
+
+  public void diminuirCanal () {
+    canal--;
+}
+
+    
+}
